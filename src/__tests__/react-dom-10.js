d52293b332c50d67e37625c35d250d903f313c40
Henrik Hermansen <henrikhermansen@bekk.no>
2019-06-09 18:52:06 +0200

    Add tests 9-11

---
+++ src/__tests__/react-dom-10.js
@@ -0,0 +1,31 @@
+import React from "../react";
+import "../test-utils";
+
+class Greeting extends React.Component {
+  render() {
+    return <p>Hello world</p>;
+  }
+}
+
+test("Check Component render method returns React element", async () => {
+  const instance = new Greeting();
+  const element = instance.render();
+
+  expect(element.props.children).toEqual(["Hello world"]);
+});
+
+test("Check React Component throws error if used directly", async () => {
+  const instance = new React.Component();
+
+  expect(instance.render).not.toBeUndefined();
+  expect(typeof instance.render).toBe("function");
+
+  let error;
+  try {
+    instance.render();
+  } catch (e) {
+    error = e;
+  }
+
+  expect(error instanceof Error).toBe(true);
+});
---
+++ src/__tests__/react-dom-9.js
@@ -0,0 +1,24 @@
+import { fireEvent } from 'dom-testing-library';
+
+import React from "../react";
+import ReactDOM from "../react-dom";
+import { getExampleDOM } from "../test-utils";
+
+test("Check rendering with an event listener", async () => {
+  const container = getExampleDOM();
+  const onClick = jest.fn();
+
+  ReactDOM.render(
+    <button onClick={ onClick }>
+      Hello world!
+    </button>,
+    container
+  );
+
+  fireEvent(
+    container.querySelector("button"),
+    new MouseEvent('click')
+  );
+
+  expect(onClick).toHaveBeenCalled();
+});
