aa25da9651f4b33a2b43571a43b26cc94c246934
Henrik Hermansen <henrikhermansen@bekk.no>
2019-06-19 08:58:02 +0200

    Adjusted tests for task 11 and added tests for task 12

---
+++ src/__tests__/task-12_rerendering-state.js
@@ -0,0 +1,62 @@
+import { getNodeText, fireEvent, waitForDomChange } from 'dom-testing-library';
+
+import React from '../react';
+import ReactDOM from '../react-dom';
+import { getExampleDOM } from '../test-utils';
+
+class Greeting extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: 'world' };
+  }
+
+  render() {
+    const { name } = this.state;
+    const { newState } = this.props;
+    return (
+      <div>
+        <p>Hello {name}</p>
+        <button onClick={ () => this.setState(newState) }>
+          Expand your horizon
+        </button>
+      </div>
+    );
+  }
+}
+
+test('Check state updates correctly updates the DOM (fails with timeout if ReactDOM._reRender does not correctly defer rendering)', async () => {
+  const container = getExampleDOM();
+
+  ReactDOM.render(<Greeting newState={ { name: 'universe' } } />, container);
+
+  expect(getNodeText(container.querySelector('p'))).toBe('Hello world');
+
+  fireEvent(
+    container.querySelector('button'),
+    new MouseEvent('click')
+  );
+
+  await waitForDomChange({ container });
+
+  expect(getNodeText(container.querySelector('p'))).toBe('Hello universe');
+});
+
+test('Check DOM is not re-rendered if setState is called with null', async done => {
+  const container = getExampleDOM();
+
+  ReactDOM.render(<Greeting newState={ null } />, container);
+
+  fireEvent(
+    container.querySelector('button'),
+    new MouseEvent('click')
+  );
+
+  try {
+    await waitForDomChange({ container, timeout: 100 });
+    done.fail('Calling setState with null should not trigger a re-render');
+  } catch {
+    // We expect to catch a timeout
+  }
+
+  done();
+});
