e4639dea706c92437eb7efe78b471b8e7071f304
Hamir Mahal <hamirmahal@gmail.com>
2021-03-14 09:39:40 -0700

    close #38 by differentiating src/index.js file

--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ import React from './react';
 import ReactDOM from './react-dom';
 
 const Element = <div>Hello World!</div>;
-
+// Looking to complete the first task?
+// This is the wrong file for that!
+// To complete the first task, go to src/react/index.js.
+// This is src/index.js.
 ReactDOM.render(<Element />, document.getElementById('root'));
