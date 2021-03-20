128801f81a3390da2ceafde66a584eb4fb8a70f0
Hamir Mahal <hamirmahal@gmail.com>
2021-03-20 09:24:42 -0700

    add instruction on how to view changes to index.js

--- src/index.js
+++ src/index.js
@@ -6,4 +6,7 @@ const Element = <div>Hello World!</div>;
 // This is the wrong file for that!
 // To complete the first task, go to src/react/index.js.
 // This is src/index.js.
+// You can view changes to this file by editing it, running
+// npm start, and visiting http://localhost:1234. See
+// https://github.com/sveinpg/build-your-own-react#playground.
 ReactDOM.render(<Element />, document.getElementById('root'));
